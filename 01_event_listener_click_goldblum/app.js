e8c6dd8ac8c91dc4b5f47f661d7ff3539d186337
JoelLindow <joellindow@gmail.com>
2017-12-11 20:33:03 -0700

    Jeff Goldblum Click Event Listener and Query Selector Tinkering

---
+++ 01_event_listener_click_goldblum/app.js
@@ -0,0 +1,9 @@
+// document.querySelector('.btn').addEventListener('click')
+
+
+document.querySelector('.btn').addEventListener('click',function() {
+  var picDom = document.querySelector('.goldblum');
+  picDom.style.display = 'block';
+  picDom.src = 'thereitisgoldblum.gif';
+  picDom.alt = 'goldblum gif';
+});
